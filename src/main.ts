a3e2f2e85d8b8b5279dc0eea4afb699074923958
Asa Smith <smith.asa.la@gmail.com>
2025-04-10 16:28:28 -0400

    feat: Add visitor counter handler (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
-console.log('loaded');
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser");
 
-localStorage.setItem('count', '0');
+    return worker.start();
+  } else {
+    return;
+  }
+}
+
+enableMocking().then(async () => {
+  const endpoint =
+    "https://rdt07jwj7j.execute-api.us-east-1.amazonaws.com/prod/";
+
+  async function getVisitorCount() {
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+
+      return data;
+    } catch (e) {
+      console.error(e);
+
+      return null;
+    }
+  }
+
+  const visitorCount = await getVisitorCount();
+
+  if (!visitorCount) {
+    console.log("Visitor count unavailable");
+  } else {
+    const { count } = visitorCount;
+    console.info(`total visitors: ${count}`);
+  }
+});
